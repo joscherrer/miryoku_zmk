e0c38347b8b20709f2db833dda11d6c2b5667791
Jonathan Scherrer <jonathan.s.scherrer@gmail.com>
2024-03-29 19:03:33 +0100

    undefine before redefining macros

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,6 +5,19 @@
 
 #pragma once
 
+#undef MIRYOKU_LAYOUTMAPPING_CORNE
+#undef MIRYOKU_ALTERNATIVES_BASE_QWERTY
+#undef MIRYOKU_ALTERNATIVES_TAP_QWERTY
+#undef MIRYOKU_ALTERNATIVES_NAV_VI
+#undef MIRYOKU_ALTERNATIVES_MOUSE_VI
+#undef MIRYOKU_ALTERNATIVES_MOUSE
+#undef MIRYOKU_ALTERNATIVES_MEDIA_VI
+#undef MIRYOKU_ALTERNATIVES_MEDIA
+#undef MIRYOKU_ALTERNATIVES_NUM
+#undef MIRYOKU_ALTERNATIVES_SYM
+#undef MIRYOKU_ALTERNATIVES_FUN
+#undef MIRYOKU_ALTERNATIVES_BUTTON
+
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04, K05,      K06, K07, K08, K09, K10, K11, \
      K12, K13, K14, K15, K16, K17,      K18, K19, K20, K21, K22, K23, \
@@ -15,11 +28,6 @@ K00  K01  K02  K03  K04  K05       K06  K07  K08  K09  K10  K11  \
 K12  K13  K14  K15  K16  K17       K18  K19  K20  K21  K22  K23  \
 K24  K25  K26  K27  K28  K29       K30  K31  K32  K33  K34  K35  \
                K39  K40  K41       K42  K43  K44
-
-// XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX
-// XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX
-// XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX
-//                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
 U_NU,              &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,                    U_NU,     \
