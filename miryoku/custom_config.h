ad92d2cb46a6508f7334f9dc1529d35c6be46338
Jonathan Scherrer <jonathan.s.scherrer@gmail.com>
2025-11-15 21:57:30 +0100

    add external power toggle key

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -83,7 +83,7 @@ U_NP,              U_NP,              U_NP,              &kp ESC,           &kp 
 U_BOOT,            &u_to_U_GAME,      &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             U_NU,     \
 U_NU,              &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      U_NU,     \
 U_NU,              U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           U_NU,     \
-U_NP,              U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP,              U_NP
+&ext_power EP_TOG, U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MOUSE_VI_CORNE \
 U_BOOT,            &u_to_U_GAME,      &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             U_NU,     \
